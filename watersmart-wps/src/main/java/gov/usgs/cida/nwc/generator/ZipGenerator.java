8210c53ea2eba9aedf1675409ea46a2907f7e83a
Jordan Walker <jiwalker@usgs.gov>
2012-11-07 16:12:03 -0600

    adding in overlay for wps

---
+++ watersmart-wps/src/main/java/gov/usgs/cida/nwc/generator/ZipGenerator.java
@@ -0,0 +1,33 @@
+package gov.usgs.cida.nwc.generator;
+
+import gov.usgs.cida.nwc.binding.ZipFileBinding;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.datahandler.generator.AbstractGenerator;
+
+/**
+ *
+ * @author tkunicki
+ */
+public class ZipGenerator extends AbstractGenerator {
+
+    public ZipGenerator() {
+        supportedIDataTypes.add(ZipFileBinding.class);
+    }
+    
+    @Override
+    public InputStream generateStream(IData data, String mimeType, String schema) throws IOException {
+        if (data instanceof ZipFileBinding) {
+            Object payload = data.getPayload();
+            if (payload instanceof File) {
+                File payloadFile = (File) payload;
+                return new FileInputStream(payloadFile);
+            }
+        }
+        return null;
+    }
+
+}
