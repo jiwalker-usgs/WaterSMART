3696812db1725dc5811f1e26f91d5f5fa831cf6b
Ivan Suftin <isuftin@usgs.gov>
2012-11-22 19:28:41 -0600

    Added validation to the upload panel to check that a file is selected

--- watersmart-ui/src/main/webapp/js/pages/index/Form/IsoFormPanel.js
+++ watersmart-ui/src/main/webapp/js/pages/index/Form/IsoFormPanel.js
@@ -56,90 +56,90 @@ WaterSMART.ISOFormPanel = Ext.extend(Ext.form.FormPanel, {
             xtype: 'panel',
             layout: 'form',
             items: [
-                {
-                    fieldLabel: 'Calibration/ Validation Scenario',
-                    xtype: 'combo',
-                    mode: 'local',
-                    store: this.scenarioOptions,
-                    hiddenName: 'scenario',
-                    name: 'scenario',
-                    ref: 'scenarioField',
-                    setEditable: false,
-                    forceSelection: true,
-                    triggerAction: 'all',
-                    emptyText: 'Select a scenario...',
-                    selectOnFocus: true,
-                    value : this.create ? '' : this.scenario,
-                    anchor: '65%',
-                    listeners : {
-                        select : function (combo, record, index) {
-                            LOG.debug("got record: " + record.json);
-                            var scenario = record.get(record.fields.first().name);
-                            var maxModelVer = 0;
-                            var maxRunIdent = 0;
-                            Ext.each(this.existingVersions[scenario], function (version) {
-                                if (version.split(".")[0] == maxModelVer) {
-                                    maxRunIdent = (version.split(".")[1] > maxRunIdent) ? version.split(".")[1] : maxRunIdent;
-                                } else if (version.split(".")[0] > maxModelVer) {
-                                    maxModelVer = version.split(".")[0];
-                                    maxRunIdent = version.split(".")[1];
-                                }
-                            });
-                            if (this.create) {
-                                this.runIdentifier = ++maxRunIdent;
-                                this.modelVersion = maxModelVer;
-                                this.getForm().setValues(
-                                    {
-                                        'runIdent' : this.runIdentifier,
-                                        'version' : this.modelVersion
-                                    }
-                                );
+            {
+                fieldLabel: 'Calibration/ Validation Scenario',
+                xtype: 'combo',
+                mode: 'local',
+                store: this.scenarioOptions,
+                hiddenName: 'scenario',
+                name: 'scenario',
+                ref: 'scenarioField',
+                setEditable: false,
+                forceSelection: true,
+                triggerAction: 'all',
+                emptyText: 'Select a scenario...',
+                selectOnFocus: true,
+                value : this.create ? '' : this.scenario,
+                anchor: '65%',
+                listeners : {
+                    select : function (combo, record, index) {
+                        LOG.debug("got record: " + record.json);
+                        var scenario = record.get(record.fields.first().name);
+                        var maxModelVer = 0;
+                        var maxRunIdent = 0;
+                        Ext.each(this.existingVersions[scenario], function (version) {
+                            if (version.split(".")[0] == maxModelVer) {
+                                maxRunIdent = (version.split(".")[1] > maxRunIdent) ? version.split(".")[1] : maxRunIdent;
+                            } else if (version.split(".")[0] > maxModelVer) {
+                                maxModelVer = version.split(".")[0];
+                                maxRunIdent = version.split(".")[1];
                             }
-                            this.versionField.validate();
-                            this.runIdentField.validate();
-                        },
-                        scope : this
-                    }
-                }, {
-                    fieldLabel: 'Modeler Name',
-                    name: 'name',
-                    value : this.modelerName,
-                    allowBlank: false
-                }, {
-                    xtype : 'displayfield',
-                    fieldLabel: 'Model Name',
-                    name: 'model',
-                    value : this.modelName,
-                    allowBlank: false
-                }, {
-                    fieldLabel: 'Model Version',
-                    xtype: this.create ? 'textfield' : 'displayfield',
-                    name: 'version',
-                    ref: 'versionField',
-                    value : this.modelVersion,
-                    allowBlank: false,
-                    validator: this.validateForm
-                }, {
-                    fieldLabel: 'Run Identifier',
-                    name: 'runIdent',
-                    ref: 'runIdentField',
-                    value : this.runIdentifier,
-                    allowBlank: false,
-                    validator: this.validateForm
-                }, {
-                    xtype : 'datefield',
-                    fieldLabel: 'Run Date',
-                    name: 'creationDate',
-                    value : this.runDate,
-                    allowBlank: false
-                }, {
-                    fieldLabel: 'Comments',
-                    xtype : 'textarea',
-                    name: 'comments',
-                    value : this['abstract'],
-                    allowBlank: true,
-                    anchor: '95%'
+                        });
+                        if (this.create) {
+                            this.runIdentifier = ++maxRunIdent;
+                            this.modelVersion = maxModelVer;
+                            this.getForm().setValues(
+                            {
+                                'runIdent' : this.runIdentifier,
+                                'version' : this.modelVersion
+                            }
+                            );
+                        }
+                        this.versionField.validate();
+                        this.runIdentField.validate();
+                    },
+                    scope : this
                 }
+            }, {
+                fieldLabel: 'Modeler Name',
+                name: 'name',
+                value : this.modelerName,
+                allowBlank: false
+            }, {
+                xtype : 'displayfield',
+                fieldLabel: 'Model Name',
+                name: 'model',
+                value : this.modelName,
+                allowBlank: false
+            }, {
+                fieldLabel: 'Model Version',
+                xtype: this.create ? 'textfield' : 'displayfield',
+                name: 'version',
+                ref: 'versionField',
+                value : this.modelVersion,
+                allowBlank: false,
+                validator: this.validateForm
+            }, {
+                fieldLabel: 'Run Identifier',
+                name: 'runIdent',
+                ref: 'runIdentField',
+                value : this.runIdentifier,
+                allowBlank: false,
+                validator: this.validateForm
+            }, {
+                xtype : 'datefield',
+                fieldLabel: 'Run Date',
+                name: 'creationDate',
+                value : this.runDate,
+                allowBlank: false
+            }, {
+                fieldLabel: 'Comments',
+                xtype : 'textarea',
+                name: 'comments',
+                value : this['abstract'],
+                allowBlank: true,
+                anchor: '95%'
+            }
             ]
         }, config);
         WaterSMART.ISOFormPanel.superclass.constructor.call(this, config);
@@ -155,17 +155,39 @@ WaterSMART.ISOFormPanel = Ext.extend(Ext.form.FormPanel, {
     },
     validateForm : function () {
         var parentPanel = Ext.ComponentMgr.get('metadata-form');
-        var scenario = parentPanel.scenarioField.getValue();
+        var newScenario = parentPanel.scenarioField.getValue();
+        var origScenario = parentPanel.originalScenario;
         var newVersion = parentPanel.versionField.getValue() + "." + parentPanel.runIdentField.getValue();
         var origVersion = parentPanel.originalModelVersion + "." + parentPanel.originalRunIdentifier;
-        if ((origVersion !== newVersion || parentPanel.originalScenario !== scenario) &&
-                scenario !== "" &&
-                parentPanel.existingVersions[scenario] &&
-                parentPanel.existingVersions[scenario].indexOf(newVersion) >= 0) {
+        if ((origVersion !== newVersion || origScenario !== newScenario) &&
+            newScenario !== "" &&
+            parentPanel.existingVersions[newScenario] &&
+            parentPanel.existingVersions[newScenario].indexOf(newVersion) >= 0) {
             return "Cannot set version to one already in existance";
+        }
+        
+        return true;
+    },
+    validateUploadForm : function () {
+        var parentPanel = Ext.ComponentMgr.get('metadata-form');
+        var newScenario = parentPanel.scenarioField.getValue();
+        var origScenario = parentPanel.originalScenario;
+        var newVersion = parentPanel.versionField.getValue() + "." + parentPanel.runIdentField.getValue();
+        var origVersion = parentPanel.originalModelVersion + "." + parentPanel.originalRunIdentifier;
+        if ((origVersion !== newVersion || origScenario !== newScenario) &&
+            newScenario !== "" &&
+            parentPanel.existingVersions[newScenario] &&
+            parentPanel.existingVersions[newScenario].indexOf(newVersion) >= 0) {
+            return "Cannot set version to one already in existance";
+        }
+        
+        if (Ext.getCmp('uploadPanel') && !Ext.getCmp('uploadPanel').form.getFieldValues()['file-path']){
+            return "You have not selected a file to upload";
         }
         return true;
     }
+    
+    
 });
 
 // http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
