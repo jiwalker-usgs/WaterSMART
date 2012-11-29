1c69245119bb7613221e1e7186f1809d5cc356c2
Ivan Suftin <isuftin@usgs.gov>
2012-11-29 08:11:03 -0600

    Removed Tomcat from pom config, added error checking to SOS GetCaps in the front end

--- watersmart-ui/src/main/webapp/js/pages/index/Plotter/SOSController.js
+++ watersmart-ui/src/main/webapp/js/pages/index/Plotter/SOSController.js
@@ -11,6 +11,7 @@ WaterSMART.SOSController = Ext.extend(Ext.util.Observable, {
         );
     },
     getCaps : function (url) {
+        LOG.debug('WaterSMART.SOSController::getCaps()');
         this.loading = true;
         var sos = new OpenLayers.SOSClient({
             url: url,
@@ -18,11 +19,13 @@ WaterSMART.SOSController = Ext.extend(Ext.util.Observable, {
         });
     },
     capsLoaded : function (args) {
+        LOG.debug('WaterSMART.SOSController::capsLoaded()');
         this.loading = false;
         LOG.debug("SOS GetCapabilites store loaded");
     },
     getOffering : function (offering) {
-        if (!this.sosGetCaps.contents) {
+        LOG.debug('WaterSMART.SOSController::getOfferring()');
+        if (!this.sosGetCaps || !this.sosGetCaps.contents) {
             return undefined;
         }
         return this.sosGetCaps.contents.offeringList[offering];
@@ -32,9 +35,9 @@ WaterSMART.SOSController = Ext.extend(Ext.util.Observable, {
 OpenLayers.SOSClient = OpenLayers.Class({
     url: null,
     controller: null,
-//    map: null,
     capsformat: new OpenLayers.Format.SOSCapabilities(),
     initialize: function (options) {
+        LOG.debug('WaterSMART.SOSClient::initialize()');
         OpenLayers.Util.extend(this, options);
         var params = {'service': 'SOS', 'request': 'GetCapabilities', 'version': '1.0.0'};
         var paramString = OpenLayers.Util.getParameterString(params);
@@ -43,12 +46,14 @@ OpenLayers.SOSClient = OpenLayers.Class({
             url: url,
             success: this.parseSOSCaps,
             failure: function(response) {
-                LOG.debug(response);
+                LOG.debug('Loading SOS GetCaps failed: ' + response.responseText);
+                this.controller.loading = false;
             },
             scope: this
         });
     },
     getFois: function() {
+        LOG.debug('WaterSMART.SOSClient::getFois()');
         var result = [];
         this.offeringCount = 0;
         for (var name in this.SOSCapabilities.contents.offeringList) {
@@ -64,18 +69,21 @@ OpenLayers.SOSClient = OpenLayers.Class({
         return result;
     },
     parseSOSCaps: function(response) {
+        LOG.debug('WaterSMART.SOSClient::parseSOSCaps()');
         // cache capabilities for future use
         this.SOSCapabilities = this.capsformat.read(response.responseXML || response.responseText);
         this.controller.sosGetCaps = this.SOSCapabilities;
         this.controller.capsLoaded();
     },
     getTitleForObservedProperty: function(property) {
+        LOG.debug('WaterSMART.SOSClient::getTitleForObservedProperty()');
         for (var name in this.SOSCapabilities.contents.offeringList) {
             var offering = this.SOSCapabilities.contents.offeringList[name];
             if (offering.observedProperties[0] === property) {
                 return offering.name;
             }
         }
+        return '';
     },
     destroy: function () {
     },
