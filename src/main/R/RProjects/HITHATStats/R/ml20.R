1ce83b894a1fc3d9803ee3f4f886cbcedeec381a
JLT <jlthomps@usgs.gov>
2013-01-04 14:56:27 -0600

    fixed error

--- src/main/R/RProjects/HITHATStats/R/ml20.R
+++ src/main/R/RProjects/HITHATStats/R/ml20.R
@@ -21,10 +21,10 @@ ml20 <- function(x) {
   sets <- c(1:numsets)
   sets_merge <- as.data.frame(sort(rep.int(sets,5))[1:nrow(x)],stringsAsFactors=FALSE)
   merge_data <- cbind(sub_flow,sets_merge)
-  colnames(merge_data) <- c("date","discharge","month_val","year_val","day_val","jul_val","wy_val","seq_num")
+  colnames(merge_data) <- c("wy_val","date","discharge","month_val","year_val","day_val","jul_val","num_samples","seq_num")
   min5day <- aggregate(merge_data$discharge,list(merge_data$seq_num),FUN=min,na.rm=TRUE)
   merge_data <- merge(min5day,merge_data,by.x="Group.1",by.y="seq_num")
-  colnames(merge_data) <- c("seq_num","base_flow","date","discharge","month_val","year_val","day_val","jul_val","wy_val")
+  colnames(merge_data) <- c("seq_num","base_flow","wy_val","date","discharge","month_val","year_val","day_val","jul_val","num_samples")
   base_flows <- unique(merge_data[, c("seq_num","base_flow")])
   base_flows$base_flow <- 0.9*base_flows$base_flow
   base_flows_lag <- unique(merge_data[, c("seq_num","base_flow")])
