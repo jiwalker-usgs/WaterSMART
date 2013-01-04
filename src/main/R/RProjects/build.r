c2945eaec1fe9652dd6f193a78e86b0bc8843cfc
JLT <jlthomps@usgs.gov>
2013-01-04 14:53:34 -0600

    added new .r files

--- src/main/R/RProjects/build.r
+++ src/main/R/RProjects/build.r
@@ -1,12 +1,15 @@
 # Build the package:
 library(devtools)
-setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/")
+setwd("C:/Users/jlthomps/Desktop/git/WaterSMART/src/main/R/RProjects/")
+# setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/")
 load_all("HITHATStats/",reset = TRUE)
-setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/HITHATStats")
+setwd("C:/Users/jlthomps/Desktop/git/WaterSMART/src/main/R/RProjects/HITHATStats")
+# setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/HITHATStats")
 document()
 check()  
 run_examples()
-# test()   #Assumes testthat type tests in GLRI/inst/tests
-setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/")
+# test()   Assumes testthat type tests in GLRI/inst/tests
+setwd("C:/Users/jlthomps/Desktop/git/WaterSMART/src/main/R/RProjects/")
+# setwd("/users/jlthomps/WaterSMART/src/main/R/RProjects/")
 build("HITHATStats")
 install("HITHATStats")
